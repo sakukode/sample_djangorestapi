bde54582c82e9c7a7ef8f198561c7ea52b2f3083
Rizqi Maulana <sakukode@gmail.com>
2018-03-31 09:46:00 +0700

    DRF POST One to Many_

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 
 from ..models import Courses
 from ..serializers import CoursesSerializer
@@ -15,3 +16,9 @@ class CoursesViewSet(viewsets.ModelViewSet):
     }
     filter_fields = ('title', 'price',)
     search_fields = ('title', 'subtitle', 'price')
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
--- courses/serializers.py
+++ courses/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from user.serializers import UserSerializer
@@ -6,8 +7,33 @@ from .models import Courses
 
 class CoursesSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(required=False)
+    created_by_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
 
     class Meta:
         model = Courses
-        fields = ('__all__')
+        fields = (
+            'id',
+            'title',
+            'subtitle',
+            'description',
+            'video_url',
+            'price',
+            'image_url',
 
+            'created_by_id',
+            'created_by',
+        )
+
+    def create(self, validated_data):
+        created_by_id = validated_data.get('created_by_id')
+        validated_data.pop('created_by_id', None)
+
+        # Once you are done, create the instance with the validated data
+        course = Courses(**validated_data)
+
+        # relate with user
+        user = User.objects.filter(id=created_by_id).first()
+        course.created_by = user
+        course.save()
+
+        return course
